10834c6d6bb9237bd99260e835fa57e5e19c5e80
Ronnel <ronnelabundo@gmail.com>
2023-03-10 11:09:38 +0800

    Add H.A on String and Array Methods Manipulation

--- Refocus H.A Manipulate String and Array Method/trial.js
+++ Refocus H.A Manipulate String and Array Method/trial.js
@@ -6,22 +6,17 @@ const arr = [
     "Refocus",
     "Awesome",
 ];
+// const count ={};
+// arr.map(function (element) {
+//     count[element] = (count[element] || 0) + 1; // Short circuit evaluation
+// });
 
-const objArr = {};
-let count = 0;
-arr.map( function (element) {
-    if (element === "Web developer") {
-        count += 1
-        return objArr["Web developer"] = count;
-    }
-    else if (element === "Refocus") {
-        count += 1
-        return objArr["Refocus"] = count;
-    }
-    else{
-        count += 1
-        return objArr["Awesome"] = count;
-    }
+// console.log(count);
+
+
+const count ={};
+arr.map(function (element) {
+    (count[element]) ? (count[element] += 1) : (count[element] = 1);// ternary operator
 });
 
-console.log(objArr);
+console.log(count);
